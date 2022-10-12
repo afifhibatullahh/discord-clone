28000167ddf55474ff41cae481435b67107933d5
afifhibatullahh <afifhibatullah59@gmail.com>
2022-10-13 00:14:04 +0700

    set main func

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database conntection failed. ", err);
+  });
