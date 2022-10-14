0ded35306f328f9bbc74db7737907fa1debad7e7
afifhibatullahh <afifhibatullah59@gmail.com>
2022-10-14 23:46:32 +0700

    setup ui dashboard

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,7 +1,25 @@
+import { styled } from "@mui/system";
 import React from "react";
+import AppBar from "./appbar/AppBar";
+import FriendsSideBar from "./friends-sidebar/FriendsSideBar";
+import Messenger from "./messenger/Messenger";
+import SideBar from "./sidebar/SideBar";
+
+const Wrapper = styled("div")({
+  width: "100%",
+  height: "100vh",
+  display: "flex",
+});
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Wrapper>
+      <SideBar />
+      <FriendsSideBar />
+      <Messenger />
+      <AppBar />
+    </Wrapper>
+  );
 };
 
 export default Dashboard;
